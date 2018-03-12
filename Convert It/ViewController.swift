d201998f3f7313a2703bb7640b35dc75f25891a0
Mark Dimeglio <unimarkit@outlook.com>
2018-03-11 22:47:12 -0700

    Initial Commit

---
+++ Convert It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Convert It
+//
+//  Created by Mark on 3/11/18.
+//  Copyright © 2018 Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
